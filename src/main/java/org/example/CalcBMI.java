98f3d6a0b76d8e287805f4e8119b0ea2331c36af
Vincent <vincentverapen2012@gmail.com>
2021-06-06 22:28:42 -0400

    Final Commit

--- src/main/java/org/example/CalcBMI.java
+++ src/main/java/org/example/CalcBMI.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class CalcBMI extends getWeightandHeight{
     public double bmi(double weight, double height){
-        double bmi = Math.round(((weight / (height*height)) * 703)*100.0/100.0);
-        return bmi;
+        return Math.round(((weight / (height*height)) * 703)*100.0)/100.0;
     }
     public void checkBMI(double BMI){
         if(BMI > 18.5 && BMI<25){
